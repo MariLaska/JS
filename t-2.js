eac380247ddd4efa40307bb0277947967af35b7b
Mari <mari.laska.vl@gmail.com>
2019-03-25 14:47:41 +0300

    add t-2

--- t-2.js
+++ t-2.js
@@ -1,7 +1,19 @@
 
-function Sum( a, b) {
-    var sum = a + b;
-    return sum;
-    }
-   alert(Sum(5,8));
-   alert(Sum("str1 ","str2 "));
+function typ( x) {
+    document.writeln('undefined, number, boolean, string, object, null:');
+  
+return (x +"-"+ typeof(x)+"<br>");
+}
+
+// alert(typ(8));
+// alert(typ("8"));
+// alert(typ(null));
+// alert(typ(NaN));
+// alert(typ(true));
+// alert(typ(undefined));
+document.writeln(typ("Hello"));
+document.writeln(typ(7));
+document.writeln(typ(undefined));
+document.writeln(typ(true));
+document.writeln(typ(NaN));
+document.writeln(typ(null));
