26b52cc93296f2de44ea6f5de9f90d592372f0e8
Mari <mari.laska.vl@gmail.com>
2019-04-02 10:08:38 +0300

    add js snezinki

---
+++ t-3.js
@@ -0,0 +1,22 @@
+function getType(type) {
+    return typeof type;
+}
+/*function typ( x) {
+    document.writeln('undefined, number, boolean, string, object, null:');
+  
+return (x +"-"+ typeof(x)+"<br>");
+}*/
+
+// alert(typ(8));
+// alert(typ("8"));
+// alert(typ(null));
+// alert(typ(NaN));
+// alert(typ(true));
+// alert(typ(undefined));
+/*document.writeln(typ("Hello"));
+document.writeln(typ(7));
+document.writeln(typ(undefined));
+document.writeln(typ(true));
+document.writeln(typ(NaN));
+document.writeln(typ(null));*/
+document.writeln(getType("Hello"));
